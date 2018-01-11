23d473c48ce487f0f00f79c4878921d9be25dd79
Tom Stuart <thomascstuart@gmail.com>
2018-01-11 09:50:01 +0000

    Char Arrays

--- C/temperatureConverter.c
+++ C/temperatureConverter.c
@@ -4,11 +4,17 @@
 #define     UPPER    1000.0
 #define     STEP     50.0
 
+float convertFarenheitToCentigrade(float farenheit);
+
+float convertFarenheitToCentigrade(float farenheit) {
+    return (5.0 / 9.0) * (farenheit - 32.0);
+}
+
 int main() {
     printf("Farenheit\tCelsius\n");
     float fahr;
     for(fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP) {
-        float celsius = (5.0 / 9.0) * (fahr - 32.0);
+        float celsius = convertFarenheitToCentigrade(fahr);
         printf("%3.0f\t\t%6.1f\n", fahr, celsius);
     }
 }
