31e9c1074aeb15612b0732928eee0973fe5c2bba
Tom Stuart <thomascstuart@gmail.com>
2018-01-22 16:02:37 +0000

    Add Structs

---
+++ C/structs.c
@@ -0,0 +1,13 @@
+int main() {
+    struct point {
+        int x;
+        int y;
+    };
+
+    struct point maxpt = {320, 200};
+
+    struct rectangle {
+        struct point p1;
+        struct point p2;
+    };
+}
