c4484e8f45bd076f32d211f46deaf4e5e8162806
Tom Stuart <thomascstuart@gmail.com>
2018-01-22 17:56:23 +0000

    Start Java Linked List

---
+++ DataStructures/SingleLinkedList.java
@@ -0,0 +1,90 @@
+// import SingleLinkedList.LinkNode;
+
+public class SingleLinkedList {
+    private LinkNode head;
+    private LinkNode tail;
+    private int size;
+
+    SingleLinkedList(int data) {
+        this.head = new LinkNode(data, null);
+        this.tail = this.head;
+        this.size = 1;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public boolean isEmpty(){
+        return this.size == 0;
+    }
+
+    public void pushFront(int value) {
+       this.head = new LinkNode(value, this.head);
+       this.size++;
+    }
+
+    public Integer popFront() {
+        LinkNode front = this.head;
+        this.head = front.next;
+        return front.data;
+    }
+
+    public void pushBack(int value) {
+        LinkNode newBack = new LinkNode(value, this.tail);
+        this.tail.next = newBack;
+        this.tail = newBack;
+        this.size++;
+    }
+
+    public Integer popBack() {
+        LinkNode back = this.tail;
+        LinkNode current = this.head;
+        LinkNode penultimate = null;
+
+        while(current.hasNext()) {
+            current = current.next;
+            if(current.next == this.tail) {
+                penultimate = current.next;
+            }
+        }
+        penultimate.next = null;
+        this.tail = penultimate;
+        return back.data;
+    }
+
+    public Integer front() {
+        return this.head.data; 
+    }
+
+    public Integer back() {
+        return this.tail.data; 
+    }
+
+
+
+    public static void main(String[] args) {
+        SingleLinkedList ll = new SingleLinkedList(1);
+        ll.isEmpty();
+        ll.size();
+        ll.pushFront(2);
+        ll.popFront();
+        ll.pushBack(2);
+        ll.popBack();
+    }
+
+
+    private class LinkNode {
+        private Integer data;
+        private LinkNode next;
+
+        LinkNode(Integer data, LinkNode next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public boolean hasNext(){
+            return this.next != null;
+        }
+    }
+}
